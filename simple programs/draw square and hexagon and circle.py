89784fe560a652b7cdda3bfbb45aaf6f02e42f46
Harsha K A <97173356+Harsha-02@users.noreply.github.com>
2023-05-04 22:46:56 +0530

    Update draw square and hexagon and circle.py

--- simple programs/draw square and hexagon and circle.py
+++ simple programs/draw square and hexagon and circle.py
@@ -4,14 +4,21 @@ trtl=turtle.Turtle()
 
 for i in range(4):
   trtl.forward(25)
-  trtl.right(90)
+  trtl.left(90)
   
 trtl.penup()
-trtl.forward(100)
+trtl.backward(100)
 trtl.pendown()
 
 for i in range(6):
   trtl.forward(25)
-  trtl.right(60)
+  trtl.left(60)
   
-  turtle.done()
+trtl.penup()
+trtl.forward(200)
+trtl.pendown()
+
+trtl.circle(25)
+  
+turtle.done()
+  
